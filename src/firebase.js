dde04150c2dad0b2775ea5c2840115b0c6143ca2
ThongVM <vominhthong1603@gmail.com>
2021-04-14 11:13:02 +0700

    fn lesson 6

---
+++ src/firebase.js
@@ -0,0 +1,19 @@
+import firebase from 'firebase/app'
+import "firebase/auth"
+import "firebase/database"
+import "firebase/storage"
+
+var firebaseConfig = {
+    apiKey: "AIzaSyCbg8ojRtSZtRXOXmNtSSVWzQYcy6q5m1I",
+    authDomain: "slack-app-87d3a.firebaseapp.com",
+    projectId: "slack-app-87d3a",
+    storageBucket: "slack-app-87d3a.appspot.com",
+    messagingSenderId: "954332962267",
+    appId: "1:954332962267:web:8957d69a77b35c455a1bfe",
+    measurementId: "G-YZ0CLXLCQN"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
+
+export default firebaseConfig;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import App from './components/App';
+import Login from './components/Auth/Login'
+import Register from './components/Auth/Register'
 import reportWebVitals from './reportWebVitals';
+
+import 'semantic-ui-css/semantic.min.css'
+
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+
+const Root = () => (
+  <Router>
+    <Switch>
+      <Route exact path="/" component={App}/>
+      <Route path="/login" component={Login}/>
+      <Route path="/register" component={Register}/>
+    </Switch>
+  </Router>
+)
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Root />
   </React.StrictMode>,
   document.getElementById('root')
 );
